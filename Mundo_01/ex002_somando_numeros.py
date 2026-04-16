969959963b0875918f064950d732c9c092aeb839
AlexandreRSouza <alexandre.rsouza@yahoo.com.br>
2026-04-16 15:39:57 -0300

    inclusão de dois exexcicios

---
+++ Mundo_01/ex002_somando_numeros.py
@@ -0,0 +1,4 @@
+n1 = int (input('Digite o primeiro número: '))
+n2 = int (input('Digite o segundo número: '))
+soma = n1 + n2
+print('A soma entre o primeiro número {} e o segundo número {} é um total de {}'.format(n1, n2, soma))
