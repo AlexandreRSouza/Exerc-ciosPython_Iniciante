f0fc7a6736adcc5e7979f4fe5470aa7917e0f523
AlexandreRSouza <alexandre.rsouza@yahoo.com.br>
2026-04-17 18:56:28 -0300

    segundo dia
    segundo dia de estudo de python

---
+++ Mundo_01/ex005_antecessor_sucessor.py
@@ -0,0 +1,4 @@
+num = int(input('Digite um número: '))
+
+print('o antecessor é {an}'.format(an = num - 1))
+print('O sucessor é {su}'.format(su = num + 1))
---
+++ Mundo_01/ex006_dobro_triplo_raiz.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um número: '))
+
+print('O dobro é {dob}'.format(dob = num * 2))
+print('O triplo é {tri}'.format(tri = num * 3))
+print('A raiz quadrada é {raiz}'.format(raiz = num**(1/2)))
